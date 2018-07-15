90f5d1fc598a5b41b197262aac29639a9d6c51ab
Gallaher26Brel <superg2695@hotmail.com>
2018-07-15 02:15:47 -0400

    Properly displaying search results in gridview

--- app/src/main/java/com/codepath/newyorktimesarticlesearch/ArticleArrayAdapter.java
+++ app/src/main/java/com/codepath/newyorktimesarticlesearch/ArticleArrayAdapter.java
@@ -1,7 +1,18 @@
 package com.codepath.newyorktimesarticlesearch;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.LayoutInflaterFactory;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -9,5 +20,36 @@ public class ArticleArrayAdapter extends ArrayAdapter<Article> {
 
     public ArticleArrayAdapter (Context context, List<Article> articles){
         super(context, android.R.layout.simple_list_item_1, articles);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//        return super.getView(position, convertView, parent);
+        // get data
+        Article article = this.getItem(position);
+        // check if view is recycled
+        // if not recycled, inflate
+        if (convertView == null){
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_article_result, parent, false);
+        }
+        //find image view
+        ImageView imageView = convertView.findViewById(R.id.ivImage);
+        //clear out recycled image
+        imageView.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+
+        tvTitle.setText(article.getHeadline());
+
+        //populate thumbnail
+        String thumbnail = article.getThumbNail();
+
+        if (!TextUtils.isEmpty(thumbnail)){
+            Picasso.with(getContext()).load(thumbnail).into(imageView);
+        }
+
+        return convertView;
     }
 }
